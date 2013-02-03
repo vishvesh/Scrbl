958f2018d671d91f7db89b2d3414510f63b435fc
fragbait14 <deshmukh.vish@yahoo.com>
2013-02-02 20:28:29 -0800

    First Scrbl Commit

---
+++ WebContent/actionView/success.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+<title>Scrbl</title>
+</head>
+<body>
+   Hello, <s:property value="name"/>
+</body>
+</html>
---
+++ src/com/scrbl/action/BaseAction.java
@@ -0,0 +1,44 @@
+package com.scrbl.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport implements ServletRequestAware {
+
+	/**
+	 * @author Vishvesh Deshmukh / Amit Mhatre
+	 * Created : 2nd February, 2013
+	 * Project : Scrbl
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String firstBlood()
+	{
+		name = "Welcome To Scrbl!";
+		return SUCCESS;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
