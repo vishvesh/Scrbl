5dff35064db34524f602bb5bc7f789643bf29f56
vishvesh <fragbait14@gmail.com>
2013-02-26 21:42:15 -0800

    Added ajaxloader and sample ajax call(Both commented out scripts!).

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -4,3 +4,26 @@
       container.draggable();
     }
   );
+  
+  
+/*$.ajax({
+	type: "POST",
+	url: panel.requestUri,
+	data: "currentPage=" + panel.page,
+	success: function(data){
+		$(currentProductTab).html(data);
+		panel.pagesArray[panel.page] = data;
+		panel.page++;
+	}
+});	
+
+var createAjaxLoader = function()
+{
+	var $_div = $("<div>");
+	$_div.addClass("ajaxLoader");
+	var $_img = $("<img>");
+	$_img.attr("alt", "loading");
+	$_img.attr("src", "images/siteRedesignBeta/ajax-loader.gif");
+	$_div.html($_img);
+	return $_div;
+};*/
