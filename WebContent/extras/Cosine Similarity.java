ed7249004ec7bb67599b351dc3c44f2525ea1e2d
vishvesh <fragbait14@gmail.com>
2013-04-24 00:31:54 -0700

    Added Cosine Similarity Class.. now to integrate and calculate the similarity with the two Velocity Vectors!

---
+++ WebContent/extras/Cosine Similarity.java
@@ -0,0 +1,28 @@
+	
+	public class CosineSimilarity {
+		static void Main() {
+			int[] vecA = { 1, 2, 3, 4, 5 };
+			int[] vecB = { 6, 7, 7, 9, 10 };
+	
+			var cosSimilarity = CalculateCosineSimilarity(vecA, vecB);
+	
+			Console.WriteLine(cosSimilarity);
+			Console.Read();
+		}
+	
+		private static double CalculateCosineSimilarity(int[] vecA, int[] vecB) {
+			var dotProduct = DotProduct(vecA, vecB);
+			var magnitudeOfA = Magnitude(vecA);
+			var magnitudeOfB = Magnitude(vecB);
+	
+			return dotProduct / (magnitudeOfA * magnitudeOfB);
+		}
+	
+		private static double DotProduct(int[] vecA, int[] vecB) {
+			// I'm not validating inputs here for simplicity.
+			double dotProduct = 0;
+			for (var i = 0; i < vecA.Length; i++) {
+				dotProduct += (vecA[i] * vecB[i]);
+			}
+		}
+	}
